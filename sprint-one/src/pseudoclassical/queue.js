312576eba7ca9d827079932ceb45796f251fb49c
Ignacio Prado <ignacio@gatogordoweb.com>
2016-02-08 16:04:35 -0600

    Implement Pseudo-classical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function(){
+	this.len = 0;
+	this.storage = {};
 };
+
+Queue.prototype.enqueue = function(val){
+	this.storage[this.len] = val;
+	this.len++;
+}
+
+Queue.prototype.dequeue = function(){
+	var dequeued;
+	if(this.len){
+		var index = 0;
+		dequeued = this.storage[index];
+		while(index < this.len - 1){
+			this.storage[index] = this.storage[index + 1];
+			index++;
+		}
+		delete this.storage[this.len - 1];
+		this.len--;
+	}
+	return dequeued;
+}
+
+Queue.prototype.size = function(){
+	return this.len;
+}
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	this.len = 0;
+	this.storage = {};
 };
+
+Stack.prototype.push = function(val){
+	this.storage[this.len] = val;
+	this.len++;
+}
+
+Stack.prototype.pop = function(){
+	var popped;
+	if(this.len){
+		popped = this.storage[this.len - 1];
+		delete this.storage[this.len - 1];
+		this.len--;
+	}
+	return popped;
+}
+
+Stack.prototype.size = function(){
+	return this.len
+}
 
 
