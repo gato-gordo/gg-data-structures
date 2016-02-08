f72965b92835a9db685e59b49bc5df13769da43d
Ignacio Prado <ignacio@gatogordoweb.com>
2016-02-08 14:10:35 -0600

    Implement functional shared stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,41 @@
 var Queue = function(){
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = { 
+      len: 0, 
+      storage: {}
+  };
+
+  someInstance.enqueue 	= queueMethods.enqueue;
+  someInstance.dequeue 	= queueMethods.dequeue;
+  someInstance.size 		= queueMethods.size;
+
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+	enqueue:  function(val){
+		this.storage[this.len] =  val;
+		this.len++;
+	},
+	dequeue: function(){
+		var dequeued;
+		var len = this.len;
+		if(len){
+			var i = 0
+			dequeued  = this.storage[i];
+			while(i < len - 1){
+				this.storage[i] = this.storage[i + 1];
+				i++;
+			}
+			delete this[len - 1];
+			this.len--;
+		}
+		return dequeued;
+	}, 
+	size: function(){
+		return this.len;
+	}
+};
 
 
 
