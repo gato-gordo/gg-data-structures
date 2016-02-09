ae51e4be7ac9c82bc56d10cb3d8a40689d422771
Ignacio Prado <ignacio@gatogordoweb.com>
2016-02-08 18:41:05 -0600

    Implement tree

--- complex-data-structures/src/tree.js
+++ complex-data-structures/src/tree.js
@@ -1,28 +1,25 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  newTree.children = [];
 
-  // your code here
-  newTree.children = null;  // fix me
+  for(var key in treeMethods){
+  	newTree[key] = treeMethods[key];
+  }
 
   return newTree;
 };
 
 
 
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+	this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
-
+	return 	this.value === target ||
+					this.children.some( child => child.contains(target) );
 };
 
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
